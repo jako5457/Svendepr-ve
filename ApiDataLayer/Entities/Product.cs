1a777a6ec07660557a0a87f41e14496281f50d2f
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-04-13 09:08:54 +0200

    added api projekt

---
+++ ApiDataLayer/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDataLayer.Entities
+{
+    public class Product
+    {
+    }
+}
