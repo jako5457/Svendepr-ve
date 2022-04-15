0f6f08601896515f3abd6aa1aad77a61495103e7
Allf3 <martinnybolvej@gmail.com>
2022-04-15 14:43:49 +0200

    save

---
+++ WebClient/Helpers/UserHelpers/ClaimHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace WebClient.Helpers.UserHelpers
+{
+    public static class ClaimHelper
+    {
+        public static string GetValueFromClaim(this IEnumerable<Claim> claims, string typevalue)
+        {
+            try
+            {
+                return claims.FirstOrDefault(c => c.Type == typevalue).Value;
+
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
