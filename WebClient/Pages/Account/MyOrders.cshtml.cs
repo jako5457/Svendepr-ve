b085fec5ca21183dc3fb9998eb60ee72f7afe36e
Allf3 <martinnybolvej@gmail.com>
2022-04-15 16:10:10 +0200

    MyOrders, BoostrapIcons

---
+++ WebClient/Pages/Account/MyOrders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebClient.Pages.Account
+{
+    public class MyOrdersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
