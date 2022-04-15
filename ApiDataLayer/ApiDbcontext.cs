f0157c19fbf4e15929655dff8a25de781f9f99b8
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-04-15 15:22:32 +0200

    Added Dbsets

--- ApiDataLayer/ApiDbcontext.cs
+++ ApiDataLayer/ApiDbcontext.cs
@@ -13,5 +13,27 @@ namespace ApiDataLayer
 
         public DbSet<Category> Categories { get; set; } = default!;
 
+        public DbSet<Company> Companys { get; set; } = default!;
+
+        public DbSet<Driver> Drivers { get; set; } = default!;
+
+        public DbSet<Employee> Employeees { get; set; } = default!;
+
+        public DbSet<Order> Orders { get; set; } = default!;
+
+        public DbSet<Product> Products { get; set; } = default!;
+
+        public DbSet<ProductRequest> ProductRequests { get; set; } = default!;
+
+        public DbSet<Warehouse> WarehouseOrders { get; set; } = default!;
+
+        public DbSet<WarehouseProduct> WarehouseProducts { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
     }
+
 }
