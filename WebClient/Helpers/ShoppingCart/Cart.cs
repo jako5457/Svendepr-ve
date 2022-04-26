4561ab509c5de4464b7ace596405cc42c99dc1c2
Allf3 <martinnybolvej@gmail.com>
2022-04-26 14:37:10 +0200

    Api Google maps working

---
+++ WebClient/Helpers/ShoppingCart/Cart.cs
@@ -0,0 +1,23 @@
+﻿namespace WebClient.Helpers.ShoppingCart
+{
+    public class Cart
+    {
+        public ICollection<Item>? items { get; set; }
+
+        public int CalTotalItems()
+        {
+            if (items.Any())
+            {
+                return items.Count();
+            }
+            return 0;
+        }
+    }
+    public class Item
+    {
+        public string? Id { get; set; }
+        public int amount { get; set; }
+    }
+}
+
+
